c7283e703f2d86044d1b567630782e12f46dc194
CGonzalez08 <98342280+CGonzalez08@users.noreply.github.com>
2022-12-04 15:08:21 -0600

    Adding recipes and making sure the time shows up on details

--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -1,39 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:homebrew_dripper/models/coffee_recipe.dart';
 import 'package:homebrew_dripper/screens/recipe_steps_screen.dart';
+import 'package:homebrew_dripper/models/recipe_step.dart';
 
-class RecipeDetailScreen extends StatelessWidget {
+class RecipeDetailScreen extends StatefulWidget {
   CoffeeRecipe recipe;
-
   RecipeDetailScreen(this.recipe);
+
+  @override
+  State<RecipeDetailScreen> createState() => _RecipeDetailScreenState();
+}
+
+class _RecipeDetailScreenState extends State<RecipeDetailScreen> {
+  int currentStep;
+  List<RecipeStep> remainingSteps;
+  int stepTimeRemaining;
+  @override
+  void initState() {
+    super.initState();
+    int totalSteps = widget.recipe.steps.length;
+    currentStep = 0;
+    remainingSteps = widget.recipe.steps;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Recipe Details"),
-      ),
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+              key: Key('Back-Button'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios, color: Color(0xff4C748B)))),
       body: ListView(
         children: [
-          Text(recipe.name, key: Key("recipeName-text")),
-          Text("${recipe.coffeeVolumeGrams}", key: Key("coffeeGrams-text")),
-          Text("${recipe.waterVolumeGrams}", key: Key("waterGrams-text")),
+          Text(widget.recipe.name, key: Key("recipeName-text")),
+          Text("${widget.recipe.coffeeVolumeGrams}",
+              key: Key("coffeeGrams-text")),
+          Text("${widget.recipe.waterVolumeGrams}",
+              key: Key("waterGrams-text")),
           ListTile(
               title: Text("Steps"),
-              trailing: recipe.totaltimeinseconds < 10
+              trailing: widget.recipe.totaltimeinseconds < 10
                   ? Text(
                       "Total: " +
-                          recipe.totaltimeinminutes.toString() +
+                          widget.recipe.totaltimeinminutes.toString() +
                           ":" +
                           "0" +
-                          recipe.totaltimeinseconds.toString(),
+                          widget.recipe.totaltimeinseconds.toString(),
                       key: Key("total-time-text"))
                   : Text(
                       "Total: " +
-                          recipe.totaltimeinminutes.toString() +
+                          widget.recipe.totaltimeinminutes.toString() +
                           ":" +
-                          recipe.totaltimeinseconds.toString(),
+                          widget.recipe.totaltimeinseconds.toString(),
                       key: Key("total-time-text"))),
+          for (RecipeStep step in remainingSteps)
+            ListTile(
+                title: Text(step.text,
+                    key: Key("step${remainingSteps.indexOf(step) + 1}-text")),
+                trailing: step.seconds < 10
+                    ? Text(
+                        "0" +
+                            step.minutes.toString() +
+                            ":" +
+                            "0" +
+                            step.seconds.toString(),
+                        key: Key(
+                            "step${remainingSteps.indexOf(step) + 1}-time-text"))
+                    : Text(
+                        "0" +
+                            step.minutes.toString() +
+                            ":" +
+                            step.seconds.toString(),
+                        key: Key(
+                            "step${remainingSteps.indexOf(step) + 1}-time-text"))),
           ElevatedButton(
             child: Text("Start", key: Key("start-text")),
             key: Key("start-btn"),
@@ -41,12 +85,17 @@ class RecipeDetailScreen extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => RecipeStepsScreen(recipe)),
+                    builder: (context) => RecipeStepsScreen(widget.recipe)),
               );
             },
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
--- test/coffee_recipe_test.dart
+++ test/coffee_recipe_test.dart
@@ -69,5 +69,112 @@ void main() {
     expect(makeTestRecipe().totaltimeinseconds, 45);
     //how do we test rejecting invalid coffee recipes?
   });
+
+  test(
+      'creates a valid coffee recipe for total recipe time greater than 60 seconds',
+      () {
+    //make a coffee recipe
+    CoffeeRecipe makeTestRecipe() {
+      List<RecipeStep> steps = [
+        RecipeStep("Add 100g of water", 10),
+        RecipeStep("Cover and wait", 15),
+        RecipeStep("Stir", 10),
+        RecipeStep("Pour 260g of water", 20),
+        RecipeStep("Cover and wait", 60),
+        RecipeStep("Stir", 10),
+        RecipeStep("Cover and wait", 90),
+      ];
+      CoffeeRecipe recipe = CoffeeRecipe(
+          "Houndstooth Coffee",
+          22,
+          360,
+          "finely ground coffee",
+          "The original recipe: makes one delicious cup",
+          steps);
+      return recipe;
+    }
+
+    //check that it has the right data
+    expect(makeTestRecipe().name, "Houndstooth Coffee");
+    expect(makeTestRecipe().coffeeVolumeGrams, 22);
+    expect(makeTestRecipe().waterVolumeGrams, 360);
+    expect(makeTestRecipe().grindSize, "finely ground coffee");
+    expect(makeTestRecipe().miscDetails,
+        "The original recipe: makes one delicious cup");
+    expect(makeTestRecipe().totaltimeinminutes, 3);
+    expect(makeTestRecipe().totaltimeinseconds, 35);
+    //how do we test rejecting invalid coffee recipes?
+  });
+
+  test(
+      'creates a valid coffee recipe for total recipe time greater than 60 seconds',
+      () {
+    //make a coffee recipe
+    CoffeeRecipe makeTestRecipe() {
+      List<RecipeStep> steps = [
+        RecipeStep("Add 300g water", 15),
+        RecipeStep("Cover and wait", 30),
+        RecipeStep("Stir", 15),
+        RecipeStep("Cover and wait", 60),
+        RecipeStep("Release the brew", 75)
+      ];
+      CoffeeRecipe recipe = CoffeeRecipe(
+          "Slight Glass",
+          22,
+          300,
+          "finely ground coffee",
+          "The original recipe: makes one delicious cup",
+          steps);
+      return recipe;
+    }
+
+    //check that it has the right data
+    expect(makeTestRecipe().name, "Slight Glass");
+    expect(makeTestRecipe().coffeeVolumeGrams, 22);
+    expect(makeTestRecipe().waterVolumeGrams, 300);
+    expect(makeTestRecipe().grindSize, "finely ground coffee");
+    expect(makeTestRecipe().miscDetails,
+        "The original recipe: makes one delicious cup");
+    expect(makeTestRecipe().totaltimeinminutes, 3);
+    expect(makeTestRecipe().totaltimeinseconds, 15);
+    //how do we test rejecting invalid coffee recipes?
+  });
+
+  test(
+      'creates a valid coffee recipe for total recipe time greater than 60 seconds',
+      () {
+    //make a coffee recipe
+    CoffeeRecipe makeTestRecipe() {
+      List<RecipeStep> steps = [
+        RecipeStep("Add 100g water", 15),
+        RecipeStep("Stir", 15),
+        RecipeStep("Cover and wait", 30),
+        RecipeStep("Add 210g water", 15),
+        RecipeStep("Stir", 15),
+        RecipeStep("Cover and wait", 180),
+        RecipeStep("Release the brew", 45)
+      ];
+      CoffeeRecipe recipe = CoffeeRecipe(
+          "Serious Eats",
+          22,
+          300,
+          "finely ground coffee",
+          "The original recipe: makes one delicious cup",
+          steps);
+      return recipe;
+    }
+
+    //check that it has the right data
+    expect(makeTestRecipe().name, "Serious Eats");
+    expect(makeTestRecipe().coffeeVolumeGrams, 22);
+    expect(makeTestRecipe().waterVolumeGrams, 300);
+    expect(makeTestRecipe().grindSize, "finely ground coffee");
+    expect(makeTestRecipe().miscDetails,
+        "The original recipe: makes one delicious cup");
+    expect(makeTestRecipe().totaltimeinminutes, 5);
+    expect(makeTestRecipe().totaltimeinseconds, 15);
+    //how do we test rejecting invalid coffee recipes?
+  });
+
   //how do we test rejecting invalid coffee recipes?
 }
